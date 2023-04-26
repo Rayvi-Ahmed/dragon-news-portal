c45d93912adf4519b4de78097f0041e8e893f846
Misbah Uddin <rayvi73@gmail.com>
2023-04-26 12:56:16 +0600

    header design complete

--- src/Sheard/Header/Header.jsx
+++ src/Sheard/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../../src/assets/logo.png'
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -17,6 +17,28 @@ const Header = () => {
                     <Marquee className='text-danger bg-senecdary' speed={100}>
                         I can be a React component, multiple React components, or just some text. !! I can be a React component, multiple React components, or just some text.
                     </Marquee>
+
+
+                </div>
+                <div className='my-5'>
+                    <Navbar className='rounded-2' collapseOnSelect expand="lg" bg="light" variant="light">
+                        <Container>
+                            <Navbar.Brand href="#home">Dragon News</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mx-auto">
+                                    <Nav.Link href="#features">Home</Nav.Link>
+                                    <Nav.Link href="#pricing">Career</Nav.Link>
+                                </Nav>
+                                <Nav>
+                                    <Nav.Link href="#deets">Profile</Nav.Link>
+                                    <Nav.Link eventKey={2} href="#memes">
+                                        <Button variant="secondary">Login</Button>
+                                    </Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
                 </div>
 
             </div>
